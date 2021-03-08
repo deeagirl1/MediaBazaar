ffacb3671700b828566c04d56379fd3ec63e0d47
I462052 <a.sava@student.fontys.nl>
2021-03-08 10:46:58 +0100

    Added Account.cs, IAccount.cs, Administrator.cs, Manager.cs, Person.cs

---
+++ C# app/MediaBazaarApp/MediaBazaarApp/Classes/AccountManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp.Classes
+{
+    class AccountManager
+    {
+        private List<IAccount> accounts;
+
+        public bool Add(IAccount account)
+        {
+            return false;
+        }
+        public bool Remove(IAccount account)
+        {
+            return false;
+        }
+        public bool isValid(IAccount account)
+        {
+            return false;
+        }
+    }
+}
---
+++ C# app/MediaBazaarApp/MediaBazaarApp/Classes/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp.Classes
+{
+    class Person : IAccount 
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public string GetLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLogin(string login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString() => firstName + " " + lastName;
+    }
+}
