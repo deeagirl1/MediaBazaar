687734999d206a7d533dc4505317540032f03ec1
Şindrilaru,Andreea A.D <a.sindrilaru@student.fontys.nl>
2021-03-10 20:00:49 +0100

    WorkShift classes added

---
+++ C# app/MediaBazaarApp/MediaBazaarApp/Classes/ShiftSchedule.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp.Classes
+{
+    public class ShiftSchedule
+    {
+        private List<WorkShift> workShifts = new List<WorkShift>();
+
+        public void Add(WorkShift shift)
+        {
+            workShifts.Add(shift);
+        }
+
+        public void Remove(WorkShift shift)
+        {
+            this.workShifts.RemoveAll(a => a.ID == shift.ID);
+           
+        }
+
+        public void Update(WorkShift shift)
+        {
+            this.workShifts.Find(a => a.ID == shift.ID);
+        }
+
+        private int getShift(WorkShift shift)
+        {
+            foreach (WorkShift s in workShifts)
+            {
+                if(s.ID == shift.ID)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+
+        public IEnumerable<WorkShift> GetShiftsInPerioad(DateTime start, DateTime end)
+        {
+            foreach (WorkShift s in workShifts)
+            {
+                if(s.date >= start && s.date <= end)
+                {
+                    yield return s;
+                }
+               
+            }
+        }
+
+
+
+
+
+        
+    }
+}
