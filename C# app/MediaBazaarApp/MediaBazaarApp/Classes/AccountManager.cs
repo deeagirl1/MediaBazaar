1857fbfdc35f5ff8cf56aaf19a3ca8920d838bf2
Şindrilaru,Andreea A.D <a.sindrilaru@student.fontys.nl>
2021-03-16 22:35:43 +0100

    CheckCredentials and GetStatus

--- C# app/MediaBazaarApp/MediaBazaarApp/Classes/AccountManager.cs
+++ C# app/MediaBazaarApp/MediaBazaarApp/Classes/AccountManager.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace MediaBazaarApp.Classes
 {
-    public class AccountManager
+    public class AccountManager : DBmanager
     {
         private List<IAccount> accounts;
 
         public AccountManager()
         {
             accounts = new List<IAccount>();
+        }
+
+        public object CheckCredentials(IAccount account)
+        {
+           // string sql = $"SELECT *";
+            MySqlParameter[] prms = new MySqlParameter[2];
+
+            string password = account.Password.GetHashCode().ToString();
+            prms[0] = new MySqlParameter("@login", account.Login);
+            prms[1] = new MySqlParameter("@password", password);
+
+            return this.ReadScalar(sql, prms);
+        }
+
+        public Object GetStatus(IAccount account)
+        {
+           // string sql = "SELECT *";
+            MySqlParameter[] prms = new MySqlParameter[1];
+            prms[0] = new MySqlParameter("@id", account.ID);
+            return this.ReadScalar(sql, prms);
         }
 
         public bool Add(IAccount account)
