1472e390e16bb66f87d8bc3a024378b6435cc125
Şindrilaru,Andreea A.D <a.sindrilaru@student.fontys.nl>
2021-03-16 14:38:40 +0100

    added dbManager and Database class

---
+++ C# app/MediaBazaarApp/MediaBazaarApp/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp
+{
+    public class DataBase
+    {
+        private string server;
+        private string dbi;
+        private string uid;
+        private string password;
+
+        public DataBase()
+        {
+            server = "remotemysql.com";
+            dbi = "MLH3bTxlO7";
+            uid = "MLH3bTxlO7";
+            password = "JIOJd7bNCZ";
+        }
+
+        public override string ToString()
+        {
+            return $"server={server};database={dbi};uid={uid};password={password};port=3306";
+        }
+    }
+}
