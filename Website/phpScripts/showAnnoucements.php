b5496b9e4ce6174c96669f6530f0c68e113ee78b
Şindrilaru,Andreea A.D <a.sindrilaru@student.fontys.nl>
2021-04-17 10:51:15 +0200

    added css for announcements

--- Website/phpScripts/showAnnoucements.php
+++ Website/phpScripts/showAnnoucements.php
@@ -10,10 +10,6 @@ require_once('classes/annoucementscontroller.class.php');
         $description = $value->GetDescription();
         
         echo '<div class="container">',
-        '<form method="POST" action="phpScripts/addToShoppingCart.php">',
-           '<button type="submit" class="passwordbtn">Add</button>',
-           '<input type = "hidden" name = "ProductID" value ='."$ID".'>',
-        '</form>',
         '<h2>',"$title",'</h2><br>',
         '<h3>',"$description",'</h3>',
         '<h4></h4>',
