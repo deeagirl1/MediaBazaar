69119789a7426777c212828cc108fe88de161a41
Rumenov,Stelian S.M <s.rumenov@student.fontys.nl>
2021-06-03 21:55:21 +0200

    not very working check in

--- Website/phpScripts/checkIn.php
+++ Website/phpScripts/checkIn.php
@@ -8,27 +8,8 @@ require_once("../classes/usercontroller.class.php");
     //$messages = new ContactController();
     $userController = new UserController();
 
-    $result = $userController->getShiftsForCheck($_SESSION['ID']);
+    $userController->checkInShift($_SESSION['ID']);
 
-    foreach($shift as $result){
-        echo $shift;
-    }
- 
-    if(isset($_POST['Topic']) &&
-   isset($_POST['Message'])
-  )
-  {
-      $topic = $_POST['Topic'];
-      $message = $_POST['Message'];
-      $sender = $userController->GetUserDetails($_SESSION['ID']);
-      
-      
-      $messages->SendMessage(new Contact($sender->GetID(),$topic,$message));
-      echo "<script>alert('Succesfull!'); document.location='../index.php?page=contact';</script>";
-    }
-    else 
-    {
-        echo "<script>alert('Something went wrong, please try again.</script>";
-    }
+    
 
 ?>
--- Website/views/attendance.php
+++ Website/views/attendance.php
@@ -2,20 +2,33 @@
 require('phpScripts/isSessionValid.php');
 require_once('classes/usercontroller.class.php');
 ?>
+
+<script>
+const handleCheckIn =(e)=>{
+    e.preventDefault();
+const radios = document.querySelector(".shift-option[checked='']");
+console.log(radios);
+}
+</script>
+
 <section>
 <link rel="stylesheet" href="css/attendance.css">
 <div class="container">
-<form action="../phpScripts/checkIn.php" method = "POST">
+<form  method = "POST">
  
     <h1>Attendance</h1>
     <hr>
-        <?php
+    
+    <?php
         $test = new UserController();
 
         $result = $test->getShiftsForCheck($_SESSION['ID']);
-        //echo $result;
+
         foreach($result as $shift){
+            
             echo $shift['Date'];
+            echo "&nbsp;";
+            echo "<input class='shift-option' type='radio' name='shift' value={$shift['Date']} ></input>";
             echo "<br>";
         }
         ?>
@@ -23,8 +36,7 @@ require_once('classes/usercontroller.class.php');
     <br>
     
     <hr>
-
-    <button type="submit" class="btn">Check in</button>
+    <button class="btn" onclick={handleCheckIn(e)}>Check in</button>
     <button type="submit" class="btn">Check out</button>
 </form>
 </div>
